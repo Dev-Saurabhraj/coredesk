0c0c65e589168933b31967c775f050b86510c437
saurabh <saurabhraj2509@gmail.com>
2026-04-07 23:22:38 +0530

    deleted depricated files and add dependencies and added profile icons and app icons

---
+++ lib/core/apiServices/apiEndpoints.dart
@@ -0,0 +1,7 @@
+class ApiEndpoints {
+  static const String login = '/auth/login';
+  static const String user = '/users/profile';
+  static const String stats = '/dashboard/stats';
+  static const String leaves = '/leaves/history';
+  static const String holidays = '/company/holidays';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:coredesk/config/dependencies.dart';
+import 'package:coredesk/app.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  await Dependencies.initialize(prefs);
+  runApp(const CoreDeskApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        body: Center(child: Text("hello")),
-      )
-    );
-  }
-}
-
